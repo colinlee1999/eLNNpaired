4bbfad066cacc9b8dff22748733bc64e734efbef
colinlee1999 <arteest@foxmail.com>
2015-12-30 10:33:58 -0500

    t_pi_prior deleted from eLNNpaired.r, default value will be used

--- test.r
+++ test.r
@@ -20,8 +20,5 @@ psi = c(delta_1, delta_2, k, lambda, nu)
 b = c(2,2,2)
 t_pi = c(0.05, 0.05, 0.90)
 
-
-t_pi_prior = c(0.33, 0.33, 0.34)
-
 es = gen_data_by_eLNNpaired(G,n,psi,t_pi)
 result = eLNNpaired(es, verbose = 1, is_sim =1, max_iteration_num_in_optim = 10000)
