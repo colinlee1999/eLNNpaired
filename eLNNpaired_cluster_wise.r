80e476d0006986f205f33729563d6d5f3c7e7105
colinlee1999 <arteest@foxmail.com>
2015-12-30 14:34:24 -0500

    move functions for eLNN inside eLNN function

--- eLNNpaired_cluster_wise.r
+++ eLNNpaired_cluster_wise.r
@@ -37,6 +37,221 @@ eLNNpaired_cluster_wise <- function(
   sum_dgl_square_by_l = apply(data_matrix_of_E_Set^2,1,sum, na.rm = TRUE)
 
   # initial values of 'psi' = (\delta_1, \delta_2, k, \lambda, \nu)
+  # lf123 returns log likelihood log p_1 log p_2 log p_3,
+  # specific return value depends on the last parameter 'category'
+  # 'psi' is the parameters, 'psi' = (\delta_1, \delta_2, k, \lambda, \nu)
+  lf123 <- function(
+    psi, 
+    sum_dgl_by_l, 
+    sum_dgl_square_by_l, 
+    n)
+  {
+    result = matrix(rep(0,length(sum_dgl_by_l)*3),length(sum_dgl_by_l),3)
+    colnames(result) = c("logf1", "logf2", "logf3")
+
+    for (category in 1:3)
+    {
+      mu_0 = exp(psi[category*4-3])
+
+      if (category == 2) mu_0 = -mu_0
+      if (category == 3) mu_0 = 0
+
+      k = psi[category*4-2]
+      alpha = exp(psi[category*4-1])
+      beta = exp(psi[category*4])
+    
+      A = n/(2*(n*k+1))
+      B_g = sum_dgl_by_l/n
+      C_g = beta + sum_dgl_square_by_l/2 - (sum_dgl_by_l)^2/(2*n)
+      D = alpha * log(beta) + lgamma(n/2+alpha) - lgamma(alpha) - n/2 * log(2*pi) - log(n*k+1)/2
+    
+      result[,category] = D - (n/2+alpha) * log (C_g + A * (mu_0 - B_g)^2)
+    }
+
+    return (result)
+  }
+
+  # it returns the expectation of \z_{g1}, \z_{g2}, \z_{g3}
+  # this function is used in E-step
+  # 'psi' is the parameters, 'psi' = (\delta_1, \delta_2, k, \lambda, \nu)
+  # 't_pi' = (\pi_1, \pi_2, \pi_3)
+  get_tilde_z <- function(
+    psi, 
+    t_pi, 
+    sum_dgl_by_l, 
+    sum_dgl_square_by_l, 
+    n)
+  {
+    logf = lf123(psi, sum_dgl_by_l, sum_dgl_square_by_l, n)
+    t1 = t_pi[1] * exp(logf[,1])
+    t2 = t_pi[2] * exp(logf[,2])
+    t3 = t_pi[3] * exp(logf[,3])
+    total = t1 + t2 + t3
+
+    result = cbind(t1, t2, t3)/total
+    return(result)
+  }
+
+  # returns expected log likelihood in question
+  # this is the objective function to be maximized
+  l_c <- function(
+    psi, 
+    t_pi, 
+    sum_dgl_by_l, 
+    sum_dgl_square_by_l, 
+    n, 
+    tilde_z, 
+    b, 
+    converge_threshold, 
+    infinity)
+  {
+    if (t_pi[1]<converge_threshold && t_pi[2]<converge_threshold) return (-infinity)
+    if (t_pi[1]<converge_threshold && t_pi[3]<converge_threshold) return (-infinity)
+    if (t_pi[2]<converge_threshold && t_pi[3]<converge_threshold) return (-infinity)
+
+    logf = lf123(psi, sum_dgl_by_l, sum_dgl_square_by_l, n)
+
+    result = 0
+    result = result + sum(tilde_z[,1] * logf[,1], na.rm = TRUE)
+    result = result + sum(tilde_z[,2] * logf[,2], na.rm = TRUE)
+    result = result + sum(tilde_z[,3] * logf[,3], na.rm = TRUE)
+    result = result + sum(tilde_z[,1] * log(t_pi[1]), na.rm = TRUE)
+    result = result + sum(tilde_z[,2] * log(t_pi[2]), na.rm = TRUE)
+    result = result + sum(tilde_z[,3] * log(t_pi[3]), na.rm = TRUE)
+
+    #with dirichlet distribution
+    result = result + lgamma(b[1]+b[2]+b[3]) - lgamma(b[1]) - lgamma(b[2]) - lgamma(b[3])
+    result = result + sum((b-1) * log(t_pi), na.rm = TRUE)
+
+    return (result)
+  }
+
+  # returns negative log likelihood in question
+  # maximizing over l_c is equivalent to minimizing over negative_l_c
+  negative_l_c <- function(
+    psi, 
+    t_pi, 
+    sum_dgl_by_l, 
+    sum_dgl_square_by_l, 
+    n, 
+    tilde_z, 
+    b, 
+    converge_threshold, 
+    infinity)
+  {
+    return (-l_c(
+      psi, 
+      t_pi, 
+      sum_dgl_by_l, 
+      sum_dgl_square_by_l, 
+      n, 
+      tilde_z, 
+      b, 
+      converge_threshold, 
+      infinity))
+  }
+
+  # returns gradient w.r.t. 'psi' = (\delta_1, \delta_2, k, \lambda, \nu)
+  gradient_l_c <- function(
+    psi, 
+    t_pi, 
+    sum_dgl_by_l, 
+    sum_dgl_square_by_l, 
+    n, 
+    tilde_z, 
+    b, 
+    converge_threshold, 
+    infinity)
+  {
+    G = length(sum_dgl_by_l)
+    gradient_temp = matrix(rep(0,12),4,3)
+
+    for (category in 1:3)
+    {
+      mu_0 = exp(psi[category*4-3])
+        k = psi[category*4-2]
+        
+        lambda = psi[category*4-1]
+        nu = psi[category*4]
+
+        alpha = exp(psi[category*4-1])
+        beta = exp(psi[category*4])
+
+
+        A = n/(2*(n*k+1))
+        B_g = sum_dgl_by_l/n
+        C_g = beta + sum_dgl_square_by_l/2 - (sum_dgl_by_l)^2/(2*n)
+        D = alpha * log(beta) + lgamma(n/2+alpha) - lgamma(alpha) -n/2 * log(2*pi) - log(n*k+1)/2
+        
+        if (category == 2) mu_0 = - mu_0
+        if (category == 3) mu_0 = 0
+
+        d_delta = - sum(tilde_z[,category] * (2*A*(mu_0 - B_g))/(A*(mu_0 - B_g)^2 + C_g) * (n/2 + alpha) * mu_0)
+
+        if (category == 3)
+        {
+          d_delta = 0
+        }
+
+        d_k = - (n * sum(tilde_z[,category])) / (2*(n*k+1)) + (n^2)/(2*(n*k+1)^2) * (n/2+alpha) *
+                sum(tilde_z[,category]*(mu_0-B_g)^2/(A*(mu_0-B_g)^2+C_g))
+        
+        d_lambda = sum(tilde_z[,category] * alpha * (nu + digamma(n/2+alpha) - digamma(alpha))) - alpha * ( sum(tilde_z[,category] * log(A * (mu_0 - B_g)^2 + C_g)) )
+        
+        d_nu = sum(tilde_z[,category]) * alpha - beta * (n/2 + alpha) * (sum(tilde_z[,category] / (A * (mu_0 - B_g)^2 + C_g)))
+        
+        gradient_temp[,category] = c(d_delta, d_k, d_lambda, d_nu)
+    }
+
+    result = c(gradient_temp)
+
+    return (result)
+  }
+
+  # returns gradient of nagative log likelihood function
+  gradient_negative_l_c <- function(
+    psi, 
+    t_pi, 
+    sum_dgl_by_l, 
+    sum_dgl_square_by_l, 
+    n, 
+    tilde_z, 
+    b, 
+    converge_threshold, 
+    infinity)
+  {
+    return (-gradient_l_c(
+      psi, 
+      t_pi, 
+      sum_dgl_by_l, 
+      sum_dgl_square_by_l, 
+      n, 
+      tilde_z, 
+      b, 
+      converge_threshold, 
+      infinity))
+  }
+
+  get_t_pi <- function(
+    psi, 
+    t_pi, 
+    sum_dgl_by_l, 
+    sum_dgl_square_by_l, 
+    n, 
+    tilde_z, 
+    b,
+    converge_threshold,
+    infinity)
+  {
+    #with Dirichlet distribution
+    denominator = length(sum_dgl_by_l) + sum(b, na.rm=TRUE) - 3
+
+    t1 = (sum(tilde_z[,1], na.rm=TRUE) + b[1] - 1) / denominator
+    t2 = (sum(tilde_z[,2], na.rm=TRUE) + b[2] - 1) / denominator
+    t3 = (sum(tilde_z[,3], na.rm=TRUE) + b[3] - 1) / denominator
+
+    return (c(t1,t2,t3))
+  }
 
   median_dgl_by_l = apply(data_matrix_of_E_Set, 1, median, na.rm=TRUE)
 
@@ -169,220 +384,4 @@ eLNNpaired_cluster_wise <- function(
     mleinfo = mleinfo, 
     t_pi = t_pi)
   invisible(result)
-}
-
-# lf123 returns log likelihood log p_1 log p_2 log p_3,
-# specific return value depends on the last parameter 'category'
-# 'psi' is the parameters, 'psi' = (\delta_1, \delta_2, k, \lambda, \nu)
-lf123 <- function(
-  psi, 
-  sum_dgl_by_l, 
-  sum_dgl_square_by_l, 
-  n)
-{
-  result = matrix(rep(0,length(sum_dgl_by_l)*3),length(sum_dgl_by_l),3)
-  colnames(result) = c("logf1", "logf2", "logf3")
-
-  for (category in 1:3)
-  {
-    mu_0 = exp(psi[category*4-3])
-
-    if (category == 2) mu_0 = -mu_0
-    if (category == 3) mu_0 = 0
-
-    k = psi[category*4-2]
-    alpha = exp(psi[category*4-1])
-    beta = exp(psi[category*4])
-  
-    A = n/(2*(n*k+1))
-    B_g = sum_dgl_by_l/n
-    C_g = beta + sum_dgl_square_by_l/2 - (sum_dgl_by_l)^2/(2*n)
-    D = alpha * log(beta) + lgamma(n/2+alpha) - lgamma(alpha) - n/2 * log(2*pi) - log(n*k+1)/2
-  
-    result[,category] = D - (n/2+alpha) * log (C_g + A * (mu_0 - B_g)^2)
-  }
-
-  return (result)
-}
-
-# it returns the expectation of \z_{g1}, \z_{g2}, \z_{g3}
-# this function is used in E-step
-# 'psi' is the parameters, 'psi' = (\delta_1, \delta_2, k, \lambda, \nu)
-# 't_pi' = (\pi_1, \pi_2, \pi_3)
-get_tilde_z <- function(
-  psi, 
-  t_pi, 
-  sum_dgl_by_l, 
-  sum_dgl_square_by_l, 
-  n)
-{
-  logf = lf123(psi, sum_dgl_by_l, sum_dgl_square_by_l, n)
-  t1 = t_pi[1] * exp(logf[,1])
-  t2 = t_pi[2] * exp(logf[,2])
-  t3 = t_pi[3] * exp(logf[,3])
-  total = t1 + t2 + t3
-
-  result = cbind(t1, t2, t3)/total
-  return(result)
-}
-
-# returns expected log likelihood in question
-# this is the objective function to be maximized
-l_c <- function(
-  psi, 
-  t_pi, 
-  sum_dgl_by_l, 
-  sum_dgl_square_by_l, 
-  n, 
-  tilde_z, 
-  b, 
-  converge_threshold, 
-  infinity)
-{
-  if (t_pi[1]<converge_threshold && t_pi[2]<converge_threshold) return (-infinity)
-  if (t_pi[1]<converge_threshold && t_pi[3]<converge_threshold) return (-infinity)
-  if (t_pi[2]<converge_threshold && t_pi[3]<converge_threshold) return (-infinity)
-
-  logf = lf123(psi, sum_dgl_by_l, sum_dgl_square_by_l, n)
-
-  result = 0
-  result = result + sum(tilde_z[,1] * logf[,1], na.rm = TRUE)
-  result = result + sum(tilde_z[,2] * logf[,2], na.rm = TRUE)
-  result = result + sum(tilde_z[,3] * logf[,3], na.rm = TRUE)
-  result = result + sum(tilde_z[,1] * log(t_pi[1]), na.rm = TRUE)
-  result = result + sum(tilde_z[,2] * log(t_pi[2]), na.rm = TRUE)
-  result = result + sum(tilde_z[,3] * log(t_pi[3]), na.rm = TRUE)
-
-  #with dirichlet distribution
-  result = result + lgamma(b[1]+b[2]+b[3]) - lgamma(b[1]) - lgamma(b[2]) - lgamma(b[3])
-  result = result + sum((b-1) * log(t_pi), na.rm = TRUE)
-
-  return (result)
-}
-
-# returns negative log likelihood in question
-# maximizing over l_c is equivalent to minimizing over negative_l_c
-negative_l_c <- function(
-  psi, 
-  t_pi, 
-  sum_dgl_by_l, 
-  sum_dgl_square_by_l, 
-  n, 
-  tilde_z, 
-  b, 
-  converge_threshold, 
-  infinity)
-{
-  return (-l_c(
-    psi, 
-    t_pi, 
-    sum_dgl_by_l, 
-    sum_dgl_square_by_l, 
-    n, 
-    tilde_z, 
-    b, 
-    converge_threshold, 
-    infinity))
-}
-
-# returns gradient w.r.t. 'psi' = (\delta_1, \delta_2, k, \lambda, \nu)
-gradient_l_c <- function(
-  psi, 
-  t_pi, 
-  sum_dgl_by_l, 
-  sum_dgl_square_by_l, 
-  n, 
-  tilde_z, 
-  b, 
-  converge_threshold, 
-  infinity)
-{
-  G = length(sum_dgl_by_l)
-  gradient_temp = matrix(rep(0,12),4,3)
-
-  for (category in 1:3)
-  {
-    mu_0 = exp(psi[category*4-3])
-      k = psi[category*4-2]
-      
-      lambda = psi[category*4-1]
-      nu = psi[category*4]
-
-      alpha = exp(psi[category*4-1])
-      beta = exp(psi[category*4])
-
-
-      A = n/(2*(n*k+1))
-      B_g = sum_dgl_by_l/n
-      C_g = beta + sum_dgl_square_by_l/2 - (sum_dgl_by_l)^2/(2*n)
-      D = alpha * log(beta) + lgamma(n/2+alpha) - lgamma(alpha) -n/2 * log(2*pi) - log(n*k+1)/2
-      
-      if (category == 2) mu_0 = - mu_0
-      if (category == 3) mu_0 = 0
-
-      d_delta = - sum(tilde_z[,category] * (2*A*(mu_0 - B_g))/(A*(mu_0 - B_g)^2 + C_g) * (n/2 + alpha) * mu_0)
-
-      if (category == 3)
-      {
-        d_delta = 0
-      }
-
-      d_k = - (n * sum(tilde_z[,category])) / (2*(n*k+1)) + (n^2)/(2*(n*k+1)^2) * (n/2+alpha) *
-              sum(tilde_z[,category]*(mu_0-B_g)^2/(A*(mu_0-B_g)^2+C_g))
-      
-      d_lambda = sum(tilde_z[,category] * alpha * (nu + digamma(n/2+alpha) - digamma(alpha))) - alpha * ( sum(tilde_z[,category] * log(A * (mu_0 - B_g)^2 + C_g)) )
-      
-      d_nu = sum(tilde_z[,category]) * alpha - beta * (n/2 + alpha) * (sum(tilde_z[,category] / (A * (mu_0 - B_g)^2 + C_g)))
-      
-      gradient_temp[,category] = c(d_delta, d_k, d_lambda, d_nu)
-  }
-
-  result = c(gradient_temp)
-
-  return (result)
-}
-
-# returns gradient of nagative log likelihood function
-gradient_negative_l_c <- function(
-  psi, 
-  t_pi, 
-  sum_dgl_by_l, 
-  sum_dgl_square_by_l, 
-  n, 
-  tilde_z, 
-  b, 
-  converge_threshold, 
-  infinity)
-{
-  return (-gradient_l_c(
-    psi, 
-    t_pi, 
-    sum_dgl_by_l, 
-    sum_dgl_square_by_l, 
-    n, 
-    tilde_z, 
-    b, 
-    converge_threshold, 
-    infinity))
-}
-
-get_t_pi <- function(
-  psi, 
-  t_pi, 
-  sum_dgl_by_l, 
-  sum_dgl_square_by_l, 
-  n, 
-  tilde_z, 
-  b,
-  converge_threshold,
-  infinity)
-{
-  #with Dirichlet distribution
-  denominator = length(sum_dgl_by_l) + sum(b, na.rm=TRUE) - 3
-
-  t1 = (sum(tilde_z[,1], na.rm=TRUE) + b[1] - 1) / denominator
-  t2 = (sum(tilde_z[,2], na.rm=TRUE) + b[2] - 1) / denominator
-  t3 = (sum(tilde_z[,3], na.rm=TRUE) + b[3] - 1) / denominator
-
-  return (c(t1,t2,t3))
 }
